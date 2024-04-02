add8977bc7a6760a89186d7f01dfd813f8becd4e
matheusmacedosantos <matheus.santos@sou.inteli.edu.br>
2024-04-02 11:35:12 -0300

    feat: app structure

--- components/HeaderApplication/HeaderApp.tsx
+++ components/HeaderApplication/HeaderApp.tsx
@@ -4,16 +4,15 @@ import logo from "@/public/logo_horizontal.svg";
 import Image from "next/image";
 import { Button } from "../ui/button";
 
-
 const HeaderApp = () => {
   return (
-    <header className="w-full flex justify-between items-center p-4  text-white fixed top-0 ">
+    <header className="w-full flex justify-between items-center p-4 text-white fixed top-0">
       <div>
         <Image src={logo} alt="logo" height={50} />
       </div>
-      <div>
-        <span className="mr-2">88°F Most Cloudly</span>
-        <span>10:30 AM 06-22-2023</span>
+      <div className="flex-1 flex justify-center items-center flex-col">
+        <h1 className="  font-semibold text-lg">10:30 AM</h1>
+        <span className="font-thin">06-22-2023</span>
       </div>
       <div>
         <Button>4AoLS...3bTHM</Button>
