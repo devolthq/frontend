56d3ad86b512fe7a0ce04d8e99f4dfdc200f84ca
matheusmacedosantos <matheus.santos@sou.inteli.edu.br>
2024-04-02 12:53:28 -0300

    fix code

--- app/application/page.tsx
+++ app/application/page.tsx
@@ -5,9 +5,7 @@ import HeaderApp from '../../components/HeaderApplication/HeaderApp';
 import SidebarApp from '../../components/SidebarApp/SidebarApp';
 import { LocationProvider } from '@/contexts/LocationContext';
 import ApplicationDashboard from '@/components/ApplicationDashboard/ApplicationDashboard';
-// Importe os componentes das outras páginas quando estiverem implementados
-// import PaginaDois from '@/components/PaginaDois';
-// import PaginaTres from '@/components/PaginaTres';
+
 
 const ApplicationHome = () => {
   const [activePage, setActivePage] = useState('dashboard');
@@ -17,10 +15,8 @@ const ApplicationHome = () => {
       case 'dashboard':
         return <ApplicationDashboard />;
       case 'paginaDois':
-        // Retorna <PaginaDois /> quando implementado
         return <div>Pagina Dois</div>;
       case 'paginaTres':
-        // Retorna <PaginaTres /> quando implementado
         return <div>Pagina Tres</div>;
       default:
         return <ApplicationDashboard />;
