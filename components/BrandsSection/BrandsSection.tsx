1ccaf46b96c9322d286c40cfae6266a9a921cd58
Emanuele Morais <emanuele.lmorais@gmail.com>
2024-04-03 10:49:23 -0300

    style: :lipstick: add new brands in brand section

--- components/BrandsSection/BrandsSection.tsx
+++ components/BrandsSection/BrandsSection.tsx
@@ -3,18 +3,22 @@ import React from "react";
 import BYD from "@/public/BYD.svg";
 import GWM from "@/public/GWM.svg";
 import Haval from "@/public/haval.svg";
+import Tesla from "@/public/tesla.svg";
+import Nissan from "@/public/nissan.svg";
 
 
 export default function BrandsSection() {
   return (
     <div className="text-center max-w-screen-xl mx-auto mb-10 lg:mb-20">
-      <h2 className="text-xl md:text-2xl mb-6 text-neutral-500 ">
-      Companies that trust DeVolt  
+      <h2 className="text-xl mb-6 text-neutral-500 ">
+      Your favorite companies trust us  
        </h2>
-      <div className="flex flex-wrap gap-6 md:gap-14 justify-center px-10">
-        <Image width={130} src={BYD} alt="car company logo"/>
+      <div className="flex flex-wrap gap-8 md:gap-14 justify-center px-10">
         <Image width={150} src={Haval} alt="car company logo"/>
-        <Image width={120} src={GWM} alt="car company logo"/>
+        <Image width={80} src={GWM} alt="car company logo"/>
+        <Image width={180} src={Tesla} alt="car company logo"/>
+        <Image width={100} src={BYD} alt="car company logo"/>
+        <Image width={130} src={Nissan} alt="car company logo"/>       
       </div>
     </div>
   );
