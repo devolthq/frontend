198142cd2c4f70e41e31a4ac3cd36584e38fa861
Emanuele Morais <emanuele.lmorais@gmail.com>
2024-04-07 20:15:00 -0300

    edit about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@
 import type { NextPage } from "next";
 import Topbar from "@/components/Topbar/Topbar";
 import "../globals.css";
-import station3d from "@/public/station-3d.svg";
+import station3d from "@/public/station3d.svg";
 import Image from "next/image";
 import { Card } from "@/components/ui/card";
 
@@ -12,25 +12,27 @@ const About: NextPage = () => {
   return (
     <>
      <Topbar/>
-        <div className="px-12 md:px-40 mt-4 md:mt-12">
-            <p className="text-5xl font-semibold">What is Devolt? Can I trust it?</p>
-            <div className="flex gap-16 items-center max-h-screen-2xl">
+        <div className="flex justify-center">
+        <div className="px-12 md:px-24 mt-4 md:mt-12 max-w-screen-2xl w-[100%]">
+            <p className="text-5xl font-semibold md:px-10 mb-2">What is Devolt? Can I trust it?</p>
+            <div className="flex items-center justify-center">
                 <div className="md:w-[70%] md:h-[80%] flex p-4 gap-8">
-                    <div className="">
-                        <Card className="md:w-[50vh] md:h-[30vh] mb-6">
+                    <div className="md:w-[50%]">
+                        <Card className="md:h-[30vh] mb-6">
                             
                         </Card>
-                        <Card className="md:w-[50vh] md:h-[30vh]">
+                        <Card className="md:h-[30vh]">
                             
                         </Card>      
                     </div>
-                    <div className="">
-                        <Card className="md:w-[50vh] md:h-full mb-6">           
+                    <div className="md:w-[50%]">
+                        <Card className="md:h-full mb-6">           
                         </Card>
                     </div>
                     
                 </div>
-                <Image src={station3d} alt="station" height={950} className="hidden md:block md:-mt-12"/>
+                <Image src={station3d} alt="station" height={700} className="hidden md:block md:-mt-12 max-h-screen-2xl "/>
+            </div>
             </div>
             
         </div>
