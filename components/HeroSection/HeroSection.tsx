7d11aca5938c20566e8f33d7db546924957e1573
Marcelo G Feitoza <feitoza.marcelo7@gmail.com>
2024-08-25 22:38:50 -0300

    fix: eletric* -> electric*

--- components/HeroSection/HeroSection.tsx
+++ components/HeroSection/HeroSection.tsx
@@ -5,10 +5,9 @@ import greenArch from "@/public/green_arch.svg";
 import { motion } from "framer-motion";
 import { Button } from "../ui/button";
 import VideoModal from "../VideoModal/VideoModal";
-import { ChevronDown, ChevronRight, PlayIcon, Sparkles } from "lucide-react"
+import { ChevronDown, ChevronRight, PlayIcon, Sparkles } from "lucide-react";
 
 export default function HeroSection() {
-
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   const openModal = () => {
@@ -22,7 +21,7 @@ export default function HeroSection() {
   const scrollToSection = (id: string) => {
     const element = document.getElementById(id);
     element?.scrollIntoView({ behavior: "smooth", block: "center" });
-  }
+  };
 
   return (
     <div className="h-fit md:mb-12 mb-8 pt-20 relative mx-auto max-w-screen-2xl">
@@ -37,13 +36,19 @@ export default function HeroSection() {
 
       <div className="relative z-1">
         <div className="text-center pt-16">
-
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 1, delay: 3 }}
-            className="bg-neutral-900 border  border-[#212c1d] shadow-lg mb-5 shadow-[#202b1c] flex font-semibold transition rounded-xl text-sm p-2 px-4 w-fit mx-auto ">
-            <Sparkles strokeWidth={1} size={20} color="#0f0" fill="#0f0" className="mr-2" />
+            className="bg-neutral-900 border  border-[#212c1d] shadow-lg mb-5 shadow-[#202b1c] flex font-semibold transition rounded-xl text-sm p-2 px-4 w-fit mx-auto "
+          >
+            <Sparkles
+              strokeWidth={1}
+              size={20}
+              color="#0f0"
+              fill="#0f0"
+              className="mr-2"
+            />
             V2 is now live!
           </motion.div>
 
@@ -53,11 +58,11 @@ export default function HeroSection() {
             transition={{ duration: 1.4 }}
             className="font-medium text-5xl lg:text-7xl text-neutral-400"
           >
-            Taking eletric <br />
+            Taking electric <br />
             vehicles{" "}
             <motion.span
               initial={{ opacity: 0 }}
-              animate={{ opacity: 1, }}
+              animate={{ opacity: 1 }}
               transition={{ duration: 1, delay: 1.7 }}
               className="font-bold text-white shadow-white drop-shadow-lg"
             >
@@ -70,16 +75,19 @@ export default function HeroSection() {
             animate={{ opacity: 1 }}
             transition={{ duration: 1, delay: 3 }}
           >
-            DeVolt is a descentralized solution focused on providing the eletricity needed for eletric cars. <span className="font-semibold">
-
-              With monetary incentives, logistical facilitations, more accessible stations, and an open, fully transparent market.
+            DeVolt is a descentralized solution focused on providing the
+            electricity needed for electric cars.{" "}
+            <span className="font-semibold">
+              With monetary incentives, logistical facilitations, more
+              accessible stations, and an open, fully transparent market.
             </span>
           </motion.p>
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 1, delay: 3 }}
-            className="flex flex-wrap justify-center mb-20 md:mb-0 mx-4 gap-10">
+            className="flex flex-wrap justify-center mb-20 md:mb-0 mx-4 gap-10"
+          >
             <Button onClick={() => scrollToSection("map")}>
               Find charging stations near you
               <ChevronRight className="ml-2 h-4 w-4" />
@@ -92,11 +100,15 @@ export default function HeroSection() {
                 delay: 3.2,
                 repeat: 7, // Para repetir indefinidamente, use Infinity
                 repeatType: "reverse",
-                ease: "easeInOut", 
-                bounce: true  
+                ease: "easeInOut",
+                bounce: true,
               }}
             >
-              <Button variant={"secondary"} onClick={openModal} className="hover:scale-105  transition shadow-lg shadow-[#3a583a]" >
+              <Button
+                variant={"secondary"}
+                onClick={openModal}
+                className="hover:scale-105  transition shadow-lg shadow-[#3a583a]"
+              >
                 Watch the video
                 <PlayIcon fill="#fff" className="ml-2 h-4 w-4" />
               </Button>
@@ -108,9 +120,19 @@ export default function HeroSection() {
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 1, delay: 3.5, repeat: 8, repeatType: "reverse" }}
+        transition={{
+          duration: 1,
+          delay: 3.5,
+          repeat: 8,
+          repeatType: "reverse",
+        }}
       >
-        <ChevronDown className="mx-auto hidden md:block p-2 md:p-0 md:mt-10 " color="#ccc" size={70} strokeWidth={1} />
+        <ChevronDown
+          className="mx-auto hidden md:block p-2 md:p-0 md:mt-10 "
+          color="#ccc"
+          size={70}
+          strokeWidth={1}
+        />
       </motion.div>
     </div>
   );
